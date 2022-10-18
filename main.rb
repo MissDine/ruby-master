974422f495c2334e8a10a94371ed6ef71055ef98
Enid Nyatichi <enidnyatichi1212@gmail.com>
2022-10-18 11:14:47 +0300

    Feature edit

---
+++ main.rb
@@ -0,0 +1,10 @@
+#Ruby is an interpretted server side scripting language.
+#It is pure object oriented
+#Everything i ruby is an insance of some class
+#Class of an object in ruby is decided at runtime
+
+
+# VARIABLES-Used to store value
+#Types of data in ruby on rails
+#strings,interger,float,boolean,objects
+#Constants are values which when declared cant be changed, they start with capital letters.
